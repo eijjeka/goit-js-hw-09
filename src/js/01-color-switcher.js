675c38c565866588d7940c13effc46e4444306e8
eijjeka <eijjeka@gmail.com>
2022-01-19 22:45:44 +0200

    done task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
 
+
+const refs = {
+    btnStart: document.querySelector("button[data-start]"),
+    btnStop: document.querySelector("button[data-stop]"),
+    body: document.querySelector('body')
+};
+
+let timeChangeBgColor = null;
+
+function onClickBtnStart() {
+    refs.btnStart.setAttribute("disabled", "disabled");
+    refs.btnStop.removeAttribute("disabled");
+        timeChangeBgColor = setInterval(() => {
+            refs.body.style.backgroundColor = getRandomHexColor();
+        }, 1000);
+}
+
+function onClickBtnStop() {
+    refs.btnStart.removeAttribute("disabled");
+    refs.btnStop.setAttribute("disabled", "disabled");
+    clearInterval(timeChangeBgColor);
+}
+        
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+    
+refs.btnStart.addEventListener('click', onClickBtnStart);
+
+refs.btnStop.addEventListener('click', onClickBtnStop);
